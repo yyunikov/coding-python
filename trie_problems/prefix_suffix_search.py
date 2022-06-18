ef7228d661edacb75052524a4f471de7949370fd
Yuriy Yunikov <yyunikov@gmail.com>
2022-06-18 17:37:33 +0200

    Trie problem description

--- trie_problems/prefix_suffix_search.py
+++ trie_problems/prefix_suffix_search.py
@@ -1,4 +1,28 @@
-from typing import Set, List, NewType
+"""
+PROBLEM
+
+Design a special dictionary with some words that searchs the words in it by a prefix and a suffix.
+
+Implement the WordFilter class:
+
+- WordFilter(string[] words) Initializes the object with the words in the dictionary.
+- f(string prefix, string suffix) Returns the index of the word in the dictionary, which has the prefix prefix and the suffix suffix. If there is more than one valid index, return the largest of them. If there is no such word in the dictionary, return -1.
+"""
+
+"""
+EXAMPLE
+
+Input
+["WordFilter", "f"]
+[[["apple"]], ["a", "e"]]
+Output
+[null, 0]
+
+Explanation
+WordFilter wordFilter = new WordFilter(["apple"]);
+wordFilter.f("a", "e"); // return 0, because the word at index 0 has prefix = "a" and suffix = 'e".
+"""
+from typing import Set, List
 
 
 class TrieNode:
