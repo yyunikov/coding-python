febe5a324b4396ae339628549774b5a207c23334
Yuriy Yunikov <yyunikov@gmail.com>
2022-08-07 20:35:46 +0200

    Descriptions

--- tree_problems/binary_tree_cameras.py
+++ tree_problems/binary_tree_cameras.py
@@ -1,5 +1,24 @@
 from typing import Optional, List, Set
 
+"""
+You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
+
+Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+Example 1:
+
+Input: root = [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+
+Example 2:
+
+
+Input: root = [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+"""
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
