a28ffb457ff71b2a3597da5c1d7b0eddd5e555f8
Yuriy Yunikov <yyunikov@gmail.com>
2022-08-10 15:59:52 +0200

    First unique char problem

---
+++ tests/first_unique_char_test.py
@@ -0,0 +1,11 @@
+import pytest as pytest
+
+from problems.first_unique_char import Solution
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [("loveleetcode", 2), ("leetcode", 0), ("aabb", -1)],
+)
+def test_solution(s: str, expected):
+    assert expected == Solution().firstUniqChar(s)
