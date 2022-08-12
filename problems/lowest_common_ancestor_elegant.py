628b5f91e3c397b6f8cb6e5be584036de7e35810
Yuriy Yunikov <yy@vgs.io>
2022-08-12 18:20:28 +0200

    More elegant solution

---
+++ problems/lowest_common_ancestor_elegant.py
@@ -0,0 +1,52 @@
+"""
+PROBLEM (This is a more elegant solution which applies to a binary search tree only)
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+"""
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
--- tests/lowest_common_ancestor_test.py
+++ tests/lowest_common_ancestor_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+import problems.lowest_common_ancestor_elegant
 from problems.lowest_common_ancestor import Solution
 from utils.tree_node import TreeNode
 
@@ -23,3 +24,25 @@ from utils.tree_node import TreeNode
 )
 def test_solution(root, p: TreeNode, q: TreeNode, expected: int):
     assert Solution().lowestCommonAncestor(root, p, q).val == expected
+
+
+@pytest.mark.parametrize(
+    "root,p,q,expected",
+    [
+        (TreeNode(2, left=TreeNode(1)), TreeNode(2), TreeNode(1), 2),
+        (TreeNode(6, left=TreeNode(2,
+                                   left=TreeNode(0), right=TreeNode(4,
+                                                                    left=TreeNode(3),
+                                                                    right=TreeNode(5)
+                                                                    )
+                                   ), right=TreeNode(8,
+                                                     left=TreeNode(7),
+                                                     right=TreeNode(9)
+                                                     )
+                  ), TreeNode(2), TreeNode(8), 6
+         ),
+    ],
+)
+def test_elegant_solution(root, p: TreeNode, q: TreeNode, expected: int):
+    assert problems.lowest_common_ancestor_elegant.Solution()\
+               .lowestCommonAncestor(root, p, q).val == expected
