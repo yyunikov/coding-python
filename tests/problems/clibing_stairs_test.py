95a114e27c1a5a0db0152d2dbfbc43cbdcd9e359
Yuriy Yunikov <yyunikov@gmail.com>
2022-08-20 14:02:52 +0200

    Climbing stairs 2

--- tests/problems/clibing_stairs_test.py
+++ tests/problems/clibing_stairs_test.py
@@ -2,6 +2,8 @@ import pytest
 
 from src.problems.climbing_stairs import Solution
 
+from src.problems.climbing_stairs_2 import Solution as Solution2
+
 test_parameters = [
     (1, 1),
     (2, 2),
@@ -17,3 +19,10 @@ test_parameters = [
 def test_solution(num, expected):
     assert Solution().climbStairs(num) == expected
 
+
+@pytest.mark.parametrize(
+    "num,expected",
+    test_parameters,
+)
+def test_solution_2(num, expected):
+    assert Solution2().climbStairs(num) == expected
