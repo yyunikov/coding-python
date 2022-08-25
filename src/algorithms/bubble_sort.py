b42e7924a0a2451613a3f7951277673f4228dd54
Yuriy Yunikov <yyunikov@gmail.com>
2022-08-25 20:11:06 +0200

    Optimized bubble sort

--- src/algorithms/bubble_sort.py
+++ src/algorithms/bubble_sort.py
@@ -5,8 +5,6 @@ def bubble_sort(n: List[int]):
     for i in range(0, len(n)):
         for j in range(i, len(n)):
             if n[j] < n[i]:
-                tmp = n[i]
-                n[i] = n[j]
-                n[j] = tmp
+                n[i], n[j] = n[j], n[i]
 
     return n
