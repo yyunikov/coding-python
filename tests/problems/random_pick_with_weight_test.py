5f0af965fb2d6d1a48cdb14b619023defd0910bb
Yuriy Yunikov <yyunikov@gmail.com>
2022-08-27 13:03:23 +0200

    Random pick with weight

---
+++ tests/problems/random_pick_with_weight_test.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+import pytest
+
+from src.problems.random_pick_with_weight import Solution
+
+
+@pytest.mark.parametrize(
+    "w,expected",
+    [
+        ([1], {0: 1}),
+        ([1, 3], {0: 1, 1: 3})
+    ],
+)
+@pytest.mark.flaky(reruns=10)
+def test_solution(w, expected):
+    random_picker = Solution(w)
+
+    executions = defaultdict(lambda: 0)
+    for i in range(0, sum(expected.values())):
+        index = random_picker.pickIndex()
+        executions[index] += 1
+
+    assert executions == expected
