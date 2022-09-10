eba603c09b475ab2dcb178e4785c642b94912105
Yuriy Yunikov <yyunikov@gmail.com>
2022-09-10 17:54:33 +0200

    Best time to buy and sell stock

---
+++ tests/problems/best_time_to_buy_and_sell_stock_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.problems.best_time_to_buy_and_sell_stock import Solution
+
+
+@pytest.mark.parametrize(
+    "prices,expected",
+    [
+        ([7, 1, 5, 3, 6, 4], 5),
+        ([7, 6, 4, 3, 1], 0)
+    ],
+)
+def test_solution(prices, expected):
+    assert Solution().maxProfit(prices) == expected
